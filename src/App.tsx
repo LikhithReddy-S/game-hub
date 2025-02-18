65ebda9bf3e836182e06f588f622e5301542bd66
LikhithReddy-S <slikhithreddy22@gmail.com>
2025-02-18 13:01:58 +0530

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { aside, nav } from "framer-motion/client";
+
 function App() {
   return (
-    <><Button colorScheme='blue'>Button</Button></>
-    
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "menu"`,
+        lg: `"nav nav" "aside menu"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"orange"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"green"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"menu"} bg={"blue"}>
+        Menu
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
