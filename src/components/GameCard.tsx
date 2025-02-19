efdfe9eeca0e969a2b6fabec723d58cef3b0eee1
LikhithReddy-S <slikhithreddy22@gmail.com>
2025-02-19 15:50:07 +0530

    show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,4 @@
-import { Badge, Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react'
 import { Game } from '../hooks/useGames'
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
@@ -11,7 +10,7 @@ interface props{
 
 function GameCard({game}:props) {
   return (
-    <Card borderRadius={10} overflow={'hidden'}>
+    <Card borderRadius={10} overflow={'hidden'} width='300px'>
         <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
