273e1bfa215c6f6d95eebfefb845cc54056f44a1
LikhithReddy-S <slikhithreddy22@gmail.com>
2025-02-25 20:27:08 +0530

    Displaying the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -7,14 +7,14 @@ const GenreList = () => {
   return (
     <List>
       {data.map((genre) => (
-        <ListItem key={genre.id}>
+        <ListItem key={genre.id} paddingY='5px'>
           <HStack>
             <Image
               src={getCroppedImageUrl(genre.image_background)}
               boxSize="32px"
               borderRadius={8}
             />
-            <Text>{genre.name}</Text>
+            <Text fontSize='lg'>{genre.name}</Text>
           </HStack>
         </ListItem>
       ))}
